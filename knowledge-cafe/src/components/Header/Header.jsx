785dfb57bd20b9c662a7992e24575fe79170f279
Meehnaaz Basit <mhnzbasit.0002@gmail.com>
2024-03-13 00:17:07 +0600

    added header

---
+++ knowledge-cafe/src/components/Header/Header.jsx
@@ -0,0 +1,11 @@
+const Header = () => {
+  return (
+    <div className="container mx-auto max-w-[1200px] w-[90%] lg-w[84%] py-2 md:py-4 lg:py-6">
+      <h1 className="text-2xl md:text-3xl lg:text-4xl text-blue-700 font-bold">
+        Knowledge Cafe
+      </h1>
+    </div>
+  );
+};
+
+export default Header;
